be45a827c098f433ebfb5eb26934772e7a796e3a
Ahmad Siddiqui <ahmad.siddiqui024@gmail.com>
2024-02-18 21:14:38 -0800

    final commit
    created test cases and ran them successfully

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,55 @@
-
+import java.lang.String;
 public class BankAccount {
+	private String firstName;
+	private String lastName;
+	private int accountID;
+	private double balance;
+	
+	public BankAccount() {
+		balance = 0;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void withdrawal(double amount) {
+		balance -= amount;
+	}
+	
+	public void accountSummary() {
+		System.out.println
+			("Account holder name: " + firstName + " " + lastName);
+		System.out.println
+			("Account Balance: " + balance);
+	}
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAccountID() {
+		return accountID;
+	}
+	public void setAccountID(int accountID) {
+		this.accountID = accountID;
+	}
+	public double getBalance() {
+		return balance;
+	}
+//not to be used
+//	public void setBalance(double balance) {
+//		this.balance = balance;
+//	}
+	
 
 }
--- src/CheckingAccount.java
+++ src/CheckingAccount.java
@@ -1,4 +1,22 @@
+public class CheckingAccount extends BankAccount {
+	private double interestRate;
+	
+	public void displayAccount() {
+		super.accountSummary();
+		System.out.println("Interest Rate: " + interestRate);
+	}
+	
+	public void processWithdrawal(double amount) {
+		super.withdrawal(amount);
+		if(amount > this.getBalance()) {
+			super.withdrawal(30);
+			System.out.println
+				("An overdraft fee of $30 was applied, new balance is: " + this.getBalance());
+		}
+	}
 
-public class CheckingAccount {
+	public void setInterestRate(int rate) {
+		this.interestRate = rate;
+	}
 
 }
